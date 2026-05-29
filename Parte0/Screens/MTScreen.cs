7ee7bf3f0ec5b679cd7a5ef64b9e01853d0e8fbc
Artur <arturrpiumbini@gmail.com>
2026-05-28 22:30:26 -0300

    Adição da Notação formal das MT

--- Parte0/Screens/MTScreen.cs
+++ Parte0/Screens/MTScreen.cs
@@ -10,8 +10,18 @@ public class MTScreen
         AnsiConsole.Clear();
 
         AnsiConsole.Write(
-            new FigletText("MT")
-                .Color(Color.Green));
+                new FigletText("Máquina de Turing")
+                    .Centered()
+                    .Color(Color.Cyan1));
+
+        AnsiConsole.WriteLine();
+
+        AnsiConsole.Write(
+            new Panel(
+                "[green]Artur Ribeiro Piumbini[/]")
+            .Header("[white]RESPONSAVEL[/]")
+            .Border(BoxBorder.Double)
+            .Expand());
 
         string entrada = "";
 
@@ -21,40 +31,68 @@ public class MTScreen
                 .PageSize(10)
                 .AddChoices(new[]
                 {
-                    "MT a^nb^nc^n",
-                    "MT n+1",
-                    "Sair"
+                    "Notação formal das MT",
+                    "MT aⁿbⁿcⁿ",
+                    "MT n + 1",
+                    "Voltar"
                 }));
 
         switch (opcao)
         {
-            case "MT a^nb^nc^n":
-                entrada = AnsiConsole.Ask<string>("Digite a [yellow]palavra[/] de entrada (use _ para branco): ");
-                var mt = new MT();
-                mt.EstadoInicial = "1";
-                mt.EstadosAceitacao.Add("8");
-                // (EstadoAtual, SimboloLido, NovoEstado, NovoSimbolo, Direcao)
-                mt.AdicionarTransicao("1", 'a', "2", 'X', 'D');
-                mt.AdicionarTransicao("1", '_', "8", '_', 'E');
-                mt.AdicionarTransicao("2", 'a', "2", 'a', 'D');
-                mt.AdicionarTransicao("2", 'X', "2", 'X', 'D');
-                mt.AdicionarTransicao("2", 'b', "3", 'X', 'D');
-                mt.AdicionarTransicao("3", 'b', "3", 'b', 'D');
-                mt.AdicionarTransicao("3", 'X', "3", 'X', 'D');
-                mt.AdicionarTransicao("3", 'c', "4", 'X', 'D');
-                mt.AdicionarTransicao("4", 'c', "5", 'c', 'E');
-                mt.AdicionarTransicao("4", '_', "7", '_', 'E');
-                mt.AdicionarTransicao("5", 'a', "5", 'a', 'E');
-                mt.AdicionarTransicao("5", 'b', "5", 'b', 'E');
-                mt.AdicionarTransicao("5", 'X', "5", 'X', 'E');
-                mt.AdicionarTransicao("5", '<', "6", '<', 'D');
-                mt.AdicionarTransicao("6", 'X', "6", 'X', 'D');
-                mt.AdicionarTransicao("6", 'a', "2", 'X', 'D');
-                mt.AdicionarTransicao("7", 'X', "7", 'X', 'E');
-                mt.AdicionarTransicao("7", '<', "8", '<', 'D');
-                mt.AdicionarTransicao("8", '<', "8", '<', 'D');
+            case "Notação formal das MT":
+                AnsiConsole.MarkupLine("[yellow]Notação formal das Máquinas de Turing[/]");
+                var table = new Table();
+                table.Border(TableBorder.Rounded);
+                table.AddColumn("[blue]EstadoAtual[/]");
+                table.AddColumn("[yellow]SimboloLido[/]");
+                table.AddColumn("[blue]NovoEstado[/]");
+                table.AddColumn("[yellow]NovoSimbolo[/]");
+                table.AddColumn("[grey]Direcao[/]");
 
-                Execute(entrada, mt);
+                AnsiConsole.MarkupLine("[grey]Uma Máquina de Turing consiste em uma sêxtupla (E,Σ,Γ,δ,i,F):[/]");
+                AnsiConsole.MarkupLine("[grey] Maquina reconhecedora da linguagem L4 = {aⁿbⁿcⁿ | n ≥ 1}[/]");
+                AnsiConsole.MarkupLine("[grey] E = {1,2,3,4,5,6,7,8}[/]");
+                AnsiConsole.MarkupLine("[grey] Σ = {a,b,c}[/]");
+                AnsiConsole.MarkupLine("[grey] Γ = {a,b,c,X,_,<}[/]");
+                AnsiConsole.MarkupLine("[grey] i = 1[/]");
+                AnsiConsole.MarkupLine("[grey] F = {8}[/]");
+                AnsiConsole.MarkupLine("[grey] δ:[/]");
+                table.AddRow("1", "a", "2", "X", "D");
+                table.AddRow("1", "_", "8", "_", "E");
+                table.AddRow("2", "a", "2", "a", "D");
+                table.AddRow("2", "X", "2", "X", "D");
+                table.AddRow("2", "b", "3", "X", "D");
+                table.AddRow("3", "b", "3", "b", "D");
+                table.AddRow("3", "X", "3", "X", "D");
+                table.AddRow("3", "c", "4", "X", "D");
+                table.AddRow("4", "c", "5", "c", "E");
+                table.AddRow("4", "_", "7", "_", "E");
+                table.AddRow("5", "a", "5", "a", "E");
+                table.AddRow("5", "b", "5", "b", "E");
+                table.AddRow("5", "X", "5", "X", "E");
+                table.AddRow("5", "<", "6", "<", "D");
+                table.AddRow("6", "X", "6", "X", "D");
+                table.AddRow("6", "a", "2", "X", "D");
+                table.AddRow("7", "X", "7", "X", "E");
+                table.AddRow("7", "<", "8", "<", "D");
+                table.AddRow("8", "<", "8", "<", "D");
+                AnsiConsole.Write(table);
+
+                AnsiConsole.WriteLine();
+                AnsiConsole.MarkupLine("[grey] Maquina transdutora da função f(n) = n + 1[/]");
+                AnsiConsole.MarkupLine("[grey] E = {0,1,2}[/]");
+                AnsiConsole.MarkupLine("[grey] Σ = {0,1}[/]");
+                AnsiConsole.MarkupLine("[grey] Γ = {0,1,_,<}[/]");
+                AnsiConsole.MarkupLine("[grey] i = 0[/]");
+                AnsiConsole.MarkupLine("[grey] F = {}[/]");
+                AnsiConsole.MarkupLine("[grey] δ:[/]");
+                table.Rows.Clear();
+                table.AddRow("0", "_", "1", "1", "E");
+                table.AddRow("0", "0", "1", "1", "E");
+                table.AddRow("0", "1", "0", "1", "D");
+                table.AddRow("1", "1", "1", "1", "E");
+                table.AddRow("1", "<", "2", "<", "D");
+                AnsiConsole.Write(table);
 
                 AnsiConsole.WriteLine();
                 AnsiConsole.MarkupLine("[grey]Pressione qualquer tecla para voltar ao menu...[/]");
@@ -62,19 +100,34 @@ public class MTScreen
                 MTScreen.Show();
                 break;
 
-            case "MT n+1":
+            case "MT aⁿbⁿcⁿ":
                 entrada = AnsiConsole.Ask<string>("Digite a [yellow]palavra[/] de entrada (use _ para branco): ");
-                var mti = new MT();
-                mti.EstadoInicial = "0";
-                mti.MTReconhecedor = false;
+                var mtr = new MT();
+                mtr.EstadoInicial = "1";
+                mtr.EstadosAceitacao.Add("8");
                 // (EstadoAtual, SimboloLido, NovoEstado, NovoSimbolo, Direcao)
-                mti.AdicionarTransicao("0", '_', "1", '1', 'E');
-                mti.AdicionarTransicao("0", '0', "1", '1', 'E');
-                mti.AdicionarTransicao("0", '1', "0", '1', 'D');
-                mti.AdicionarTransicao("1", '1', "1", '1', 'E');
-                mti.AdicionarTransicao("1", '<', "2", '<', 'D');
+                mtr.AdicionarTransicao("1", 'a', "2", 'X', 'D');
+                mtr.AdicionarTransicao("1", '_', "8", '_', 'E');
+                mtr.AdicionarTransicao("2", 'a', "2", 'a', 'D');
+                mtr.AdicionarTransicao("2", 'X', "2", 'X', 'D');
+                mtr.AdicionarTransicao("2", 'b', "3", 'X', 'D');
+                mtr.AdicionarTransicao("3", 'b', "3", 'b', 'D');
+                mtr.AdicionarTransicao("3", 'X', "3", 'X', 'D');
+                mtr.AdicionarTransicao("3", 'c', "4", 'X', 'D');
+                mtr.AdicionarTransicao("4", 'c', "5", 'c', 'E');
+                mtr.AdicionarTransicao("4", '_', "7", '_', 'E');
+                mtr.AdicionarTransicao("5", 'a', "5", 'a', 'E');
+                mtr.AdicionarTransicao("5", 'b', "5", 'b', 'E');
+                mtr.AdicionarTransicao("5", 'X', "5", 'X', 'E');
+                mtr.AdicionarTransicao("5", '<', "6", '<', 'D');
+                mtr.AdicionarTransicao("6", 'X', "6", 'X', 'D');
+                mtr.AdicionarTransicao("6", 'a', "2", 'X', 'D');
+                mtr.AdicionarTransicao("7", 'X', "7", 'X', 'E');
+                mtr.AdicionarTransicao("7", '<', "8", '<', 'D');
+                mtr.AdicionarTransicao("8", '<', "8", '<', 'D');
 
-                Execute(entrada, mti);
+                // Adicionar o simbolo de início da fita
+                Execute('<' + entrada, mtr);
 
                 AnsiConsole.WriteLine();
                 AnsiConsole.MarkupLine("[grey]Pressione qualquer tecla para voltar ao menu...[/]");
@@ -82,51 +135,67 @@ public class MTScreen
                 MTScreen.Show();
                 break;
 
-            case "Sair":
-                AnsiConsole.MarkupLine(
-                    "[grey]Encerrando...[/]");
+            case "MT n + 1":
+                entrada = AnsiConsole.Ask<string>("Digite a [yellow]palavra[/] de entrada (use _ para branco): ");
+                var mtt = new MT();
+                mtt.EstadoInicial = "0";
+                mtt.MTReconhecedor = false;
+                // (EstadoAtual, SimboloLido, NovoEstado, NovoSimbolo, Direcao)
+                mtt.AdicionarTransicao("0", '_', "1", '1', 'E');
+                mtt.AdicionarTransicao("0", '0', "1", '1', 'E');
+                mtt.AdicionarTransicao("0", '1', "0", '1', 'D');
+                mtt.AdicionarTransicao("1", '1', "1", '1', 'E');
+                mtt.AdicionarTransicao("1", '<', "2", '<', 'D');
+
+                // Adicionar o simbolo de início da fita
+                Execute('<' + entrada, mtt);
+
+                AnsiConsole.WriteLine();
+                AnsiConsole.MarkupLine("[grey]Pressione qualquer tecla para voltar ao menu...[/]");
+                Console.ReadKey();
+                MTScreen.Show();
+                break;
+
+            case "Voltar":
                 return;
         }
     }
 
     public static void Execute(string entrada, MT mt)
         {
-            AnsiConsole.MarkupLine("\n[grey]Iniciando simulação...[/]\n");
+            var resultado = mt.Executar(entrada);
 
-                // Adiciona o símbolo de início '<' no começo da entrada para demarcar o simbolo inicial da fita
-                var resultado = mt.Executar('<' + entrada);
+            var table = new Table();
+            table.Border(TableBorder.Rounded);
+            table.AddColumn("[blue]Passo[/]");
+            table.AddColumn("[yellow]Estado[/]");
+            table.AddColumn("Fita");
+            table.AddColumn("[grey]Pos. Cabeçote[/]");
 
-                var table = new Table();
-                table.Border(TableBorder.Rounded);
-                table.AddColumn("[blue]Passo[/]");
-                table.AddColumn("[yellow]Estado[/]");
-                table.AddColumn("Fita");
-                table.AddColumn("[grey]Pos. Cabeçote[/]");
+            foreach (var passo in resultado.historico)
+            {
+                table.AddRow(
+                    passo.Numero.ToString(),
+                    $"[yellow]{passo.Estado}[/]",
+                    Markup.Escape(passo.Fita),
+                    $"[grey]{passo.PosicaoCabecote}[/]"
+                );
+            }
 
-                foreach (var passo in resultado.historico)
-                {
-                    table.AddRow(
-                        passo.Numero.ToString(),
-                        $"[yellow]{passo.Estado}[/]",
-                        Markup.Escape(passo.Fita),
-                        $"[grey]{passo.PosicaoCabecote}[/]"
-                    );
-                }
+            AnsiConsole.Write(table);
 
-                AnsiConsole.Write(table);
+            // Exibe o resultado final formatado
+            var reconhecedor = mt.MTReconhecedor ? "Reconhecedora" : "Geradora";
+            var corPainel = resultado.aceito ? Color.Green : Color.Red;
+            var textoPainel = resultado.aceito ? "[green]ACEITO[/]" : "[red]REJEITADO[/]";
 
-                // Exibe o resultado final formatado
-                var reconhecedor = mt.MTReconhecedor ? "Reconhecedora" : "Geradora";
-                var corPainel = resultado.aceito ? Color.Green : Color.Red;
-                var textoPainel = resultado.aceito ? "[green]ACEITO[/]" : "[red]REJEITADO[/]";
+            if(reconhecedor == "Geradora")
+                textoPainel = "[green]RESULTADO GERADO[/]";
 
-                if(reconhecedor == "Geradora")
-                    textoPainel = "[green]RESULTADO GERADO[/]";
-
-                AnsiConsole.Write(
-                    new Panel($"{textoPainel}\n{resultado.motivo}")
-                        .Header("Resultado Final")
-                        .BorderColor(corPainel)
-                        .Border(BoxBorder.Double));
+            AnsiConsole.Write(
+                new Panel($"{textoPainel}\n{resultado.motivo}")
+                    .Header("Resultado Final")
+                    .BorderColor(corPainel)
+                    .Border(BoxBorder.Double));
         }
 }
